16efa611afada4e59338a68934752a11f1693a39
数海信息技术有限公司 <gn@shuhaixinxijishuyouxiangongsideMacBook-Pro.local>
2018-03-02 10:04:14 +0800

    修改QQ appID

--- anfang/src/main/java/com/xptschool/parent/XPTApplication.java
+++ anfang/src/main/java/com/xptschool/parent/XPTApplication.java
@@ -74,7 +74,7 @@ public class XPTApplication extends Application {
 
     {
         PlatformConfig.setWeixin("wx1af4f660ce9e6b37", "5bb696d9ccd75a38c8a0bfe0675559b3");
-        PlatformConfig.setQQZone("100424468", "c7394704798a158208a74ab60104f0ba");
+        PlatformConfig.setQQZone("1106232927", "c7394704798a158208a74ab60104f0ba");
         PlatformConfig.setSinaWeibo("3921700954", "04b48b094faeb16683c32669824ebdad", "http://sns.whalecloud.com");
     }
 
