285b23c7586685283ae3d6d241b3935f9390349a
数海信息 <shuhaixinxi@shuhaixinxideMac-mini.local>
2018-03-07 17:17:20 +0800

    分享二维码自动生成

--- anfang/src/main/java/com/xptschool/parent/ui/setting/QRCodeActivity.java
+++ anfang/src/main/java/com/xptschool/parent/ui/setting/QRCodeActivity.java
@@ -2,22 +2,33 @@ package com.xptschool.parent.ui.setting;
 
 import android.Manifest;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
+import android.util.Base64;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.imageaware.ImageViewAware;
 import com.umeng.socialize.ShareAction;
 import com.umeng.socialize.UMShareAPI;
 import com.umeng.socialize.UMShareListener;
 import com.umeng.socialize.bean.SHARE_MEDIA;
 import com.umeng.socialize.media.UMWeb;
 import com.xptschool.parent.R;
+import com.xptschool.parent.XPTApplication;
+import com.xptschool.parent.common.CommonUtil;
 import com.xptschool.parent.ui.main.BaseActivity;
+import com.xptschool.parent.util.QRCodeUtil;
 
 import java.util.HashMap;
+
+import butterknife.BindView;
 
 /**
  * Created by dexing on 2017-11-15 0015.
@@ -25,12 +36,34 @@ import java.util.HashMap;
 
 public class QRCodeActivity extends BaseActivity {
 
+    @BindView(R.id.QRImg)
+    ImageView QRImg;
+    String shareURL = "";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_qrcode);
         setTitle(R.string.mine_qr_code);
 
+//        Bitmap bitmap = QRCodeUtil.createQRCodeBitmap("http://school.xinpingtai.com/register?user_id=100", 100,
+//                BitmapFactory.decodeResource(getResources(), R.drawable.logo_shuhai), 0.2f);
+
+        try {
+            final String text = XPTApplication.getInstance().getCurrentUserId();
+            final byte[] textByte = text.getBytes("UTF-8");
+            //编码
+            final String encodedText = Base64.encodeToString(textByte, Base64.DEFAULT);
+
+            shareURL = "http://school.xinpingtai.com/index.php/Wap/Register/index?ref=" + encodedText;
+            Bitmap bitmap = QRCodeUtil.createCode(shareURL, BitmapFactory.decodeResource(getResources(), R.drawable.logo_shuhai));
+
+            if (bitmap != null) {
+                QRImg.setImageBitmap(bitmap);
+            }
+        } catch (Exception ex) {
+
+        }
 
         setTxtRight("分享");
         setTextRightClickListener(new View.OnClickListener() {
@@ -63,7 +96,7 @@ public class QRCodeActivity extends BaseActivity {
     }
 
     private void showShare() {
-        UMWeb web = new UMWeb(getString(R.string.label_app_download_url));
+        UMWeb web = new UMWeb(shareURL);
         web.setTitle("数海信息信平台客户端");//标题
 //        web.setThumb(thumb);  //缩略图
         web.setDescription("诚邀您下载数海信平台客户端进行体验");//描述
@@ -95,6 +128,5 @@ public class QRCodeActivity extends BaseActivity {
                     }
                 })//回调监听器
                 .open();
-
     }
 }
