50d1832cd1b06035287ea5d140c45373128272a7
数海信息 <shuhaixinxi@shuhaixinxideMac-mini.local>
2018-03-22 16:00:23 +0800

    我的页面点击手机号，进入拨号界面

--- anfang/src/main/java/com/xptschool/parent/ui/mine/role/VisitorInfoView.java
+++ anfang/src/main/java/com/xptschool/parent/ui/mine/role/VisitorInfoView.java
@@ -12,11 +12,14 @@ import android.widget.Toast;
 
 import com.android.widget.view.CircularImageView;
 import com.xptschool.parent.R;
+import com.xptschool.parent.XPTApplication;
 import com.xptschool.parent.common.SharedPreferencesUtil;
 import com.xptschool.parent.model.BeanTeacher;
 import com.xptschool.parent.model.GreenDaoHelper;
 import com.xptschool.parent.ui.mine.BaseInfoView;
 import com.xptschool.parent.util.ToastUtils;
+
+import org.w3c.dom.Text;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -36,6 +39,8 @@ public class VisitorInfoView extends BaseInfoView {
 
     @BindView(R.id.txtPhone)
     TextView txtPhone;
+    @BindView(R.id.txtRole)
+    TextView txtRole;
 
     public VisitorInfoView(Context context) {
         this(context, null);
@@ -54,27 +59,24 @@ public class VisitorInfoView extends BaseInfoView {
     }
 
     private void initData() {
-        txtName.setText(SharedPreferencesUtil.getData(mContext, SharedPreferencesUtil.KEY_USER_NAME, "").toString());
-//        txtPhone.setText();
-
+        txtName.setText(SharedPreferencesUtil.getData(mContext, SharedPreferencesUtil.KEY_VISITOR_NAME, "").toString());
+        txtPhone.setText(SharedPreferencesUtil.getData(mContext, SharedPreferencesUtil.KEY_USER_NAME, "").toString());
+        txtRole.setText(XPTApplication.getInstance().getCurrent_user_type().getRoleName());
     }
 
     @OnClick({R.id.rlMinePhoto, R.id.rlMinePhone})
     void viewClick(View view) {
-        BeanTeacher teacher = GreenDaoHelper.getInstance().getCurrentTeacher();
-        if (teacher == null) {
-            return;
-        }
         switch (view.getId()) {
             case R.id.rlMinePhone:
-                if (teacher.getPhone().isEmpty()) {
+                String phone = SharedPreferencesUtil.getData(mContext, SharedPreferencesUtil.KEY_USER_NAME, "").toString();
+                if (phone.isEmpty()) {
                     Toast.makeText(mContext, R.string.toast_phone_empty, Toast.LENGTH_SHORT).show();
                     return;
                 }
                 try {
                     Intent intent = new Intent();
                     intent.setAction(Intent.ACTION_DIAL);
-                    intent.setData(Uri.parse("tel:" + teacher.getPhone()));
+                    intent.setData(Uri.parse("tel:" + phone));
                     intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                     mContext.startActivity(intent);
                 } catch (Exception ex) {
